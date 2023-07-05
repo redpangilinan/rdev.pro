c8064e3e0f270bb99a4547339ef5964b32c32d7f
redpangilinan <redpangilinan715@gmail.com>
2023-07-05 17:22:08 +0800

    Create file for types

---
+++ types/api.d.ts
@@ -0,0 +1,11 @@
+// This file contains all types for api calls
+
+type Repo = {
+  repo: string
+  description: string
+  language: string
+  languageColor: string
+  stars: number
+  link: string
+  website: string
+}
---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = {
+  name: string
+  author: string
+  description: string
+  url: string
+  ogImage: string
+  links: {
+    github: string
+    facebook: string
+    twitter: string
+    linkedin: string
+  }
+}

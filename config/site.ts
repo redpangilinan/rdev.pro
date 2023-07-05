59dfd6b88949446c132069360997155aa2d5adf1
redpangilinan <redpangilinan715@gmail.com>
2023-07-05 17:20:57 +0800

    Create site config file

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+import { SiteConfig } from "../types"
+
+export const siteConfig: SiteConfig = {
+  name: "Red Pangilinan",
+  author: "redpangilinan",
+  description: "My personal portfolio website built in Nextjs.",
+  url: "https://redpangilinan.live",
+  ogImage: "https://redpangilinan.live/og.jpg",
+  links: {
+    github: "https://github.com/redpangilinan",
+    facebook: "https://facebook.com/redpangilinan15",
+    twitter: "https://twitter.com/_rdev7",
+    linkedin: "https://www.linkedin.com/in/jan-reynald-pangilinan",
+  },
+}

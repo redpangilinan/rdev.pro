c51baea3c4e9d1682338063931cf584b6a84d748
redpangilinan <redpangilinan715@gmail.com>
2023-07-08 18:31:31 +0800

    refactor: change contact to contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,21 @@
-import Contact from "@/components/pages/contact"
+import ContactForm from "@/components/pages/contact-form"
+import SocialMediaIcons from "@/components/common/social-media-icons"
 
 export const metadata = {
   title: "Contact",
 }
 
-export default function Dashboard() {
+export default function Contact() {
   return (
     <main className="container py-4 sm:py-8 flex flex-col items-center">
-      <Contact />
+      <div className="flex flex-col items-center text-center">
+        <h1 className="text-3xl lg:text-4xl font-bold mb-2">Contact</h1>
+        <h2 className="lg:text-lg mb-2 font-light text-zinc-500 dark:text-zinc-400">
+          Message me via social media or through email
+        </h2>
+        <SocialMediaIcons />
+      </div>
+      <ContactForm />
     </main>
   )
 }

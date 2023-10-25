f0603d527cf8946eea8058dd8c270821f8c2757f
redpangilinan <redpangilinan715@gmail.com>
2023-10-26 03:56:27 +0800

    refactor: bottom padding on footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import { siteConfig } from "@/../config/site"
 
 export default function Footer() {
   return (
-    <footer className="mt-auto block pt-4 text-sm text-zinc-500 dark:text-zinc-400">
+    <footer className="mt-auto block pb-2 pt-4 text-sm text-zinc-500 dark:text-zinc-400">
       © {new Date().getFullYear()}{" "}
       <a target="_blank" href={siteConfig.links.github} className="underline">
         Red Pangilinan

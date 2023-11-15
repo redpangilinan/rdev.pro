15983a48729b63fb08b533d5bc0490e740d28825
redpangilinan <redpangilinan715@gmail.com>
2023-11-16 06:11:59 +0800

    feat: implement dynamic rendering of discord activity

--- env.mjs
+++ env.mjs
@@ -5,15 +5,17 @@ export const env = createEnv({
   server: {
     WAKATIME_API_KEY: z.string().startsWith("waka_"),
     GH_API_URL: z.string().startsWith("https://"),
-    DISCORD_ID: z.string(),
+    DISCORD_ID: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_DISCORD_ID: z.string().min(1),
   },
   runtimeEnv: {
     WAKATIME_API_KEY: process.env.WAKATIME_API_KEY,
     GH_API_URL: process.env.GH_API_URL,
     DISCORD_ID: process.env.DISCORD_ID,
+    NEXT_PUBLIC_DISCORD_ID: process.env.NEXT_PUBLIC_DISCORD_ID,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 })

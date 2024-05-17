f950146fae2fec8ec9e17585e5396fccd1759c8a
redpangilinan <redpangilinan715@gmail.com>
2024-05-18 00:18:21 +0800

    feat(component): add timestamp for playing music in progress bar

--- src/components/timer/progress-bar.tsx
+++ src/components/timer/progress-bar.tsx
@@ -12,6 +12,15 @@ interface SpotifyBarProps {
 
 export function ProgressBar({ start, end }: SpotifyBarProps) {
   const [progress, setProgress] = React.useState(calculateProgress(start, end))
+  const [currentTime, setCurrentTime] = React.useState<number>(
+    Date.now() - start
+  )
+
+  function formatTime(timestamp: number): string {
+    const minutes = Math.floor(timestamp / 60000)
+    const seconds = ((timestamp % 60000) / 1000).toFixed(0)
+    return `${minutes}:${parseInt(seconds) < 10 ? "0" : ""}${seconds}`
+  }
 
   React.useEffect(() => {
     const intervalId = setInterval(() => {
@@ -21,9 +30,23 @@ export function ProgressBar({ start, end }: SpotifyBarProps) {
     return () => clearInterval(intervalId)
   }, [start, end])
 
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      const elapsedTime = Date.now() - start
+      setCurrentTime(elapsedTime)
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [start, end])
+
   return (
-    <div className="py-1">
-      <Progress value={progress} className="w-[200px]" />
+    <div className="flex items-center gap-2 py-1">
+      <div className="w-[30px]">{formatTime(currentTime)}</div>
+      <Progress
+        value={progress}
+        className="h-[0.5rem] w-[230px] sm:w-[300px]"
+      />
+      <div className="flex w-[30px] justify-end">{formatTime(end - start)}</div>
     </div>
   )
 }
